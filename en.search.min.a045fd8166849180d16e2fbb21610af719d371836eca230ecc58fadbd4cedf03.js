9d74a44c083734ac38e4fd2b21414b4a1f56ff7f
k-ishiyama <ishiyamax@gmail.com>
2022-04-02 11:28:34 +0900

    cleanup

--- en.search.min.a045fd8166849180d16e2fbb21610af719d371836eca230ecc58fadbd4cedf03.js
+++ en.search.min.a045fd8166849180d16e2fbb21610af719d371836eca230ecc58fadbd4cedf03.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c5ed0a810cd85976e2dfc027e2c49cf85bec68948c4a1860e54e94e437f4b2bb.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.a2a0fb28777955755fa7d3ca2a147652b6a8b46289d665c7c84186ea415264b1.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
